a994c2fe91778a37f416d9bc1b411b9c66dc7742
Yatao Li <yatao.li@live.com>
2017-11-09 13:40:28 +0800

    refactoring

--- gpu.js
+++ gpu.js
@@ -3,7 +3,11 @@
 /// Node.js script which gets the nvidia GPU usage, free memory and temp
 /// and displays it as in your tmux status bar.
 ///
-const sp = require('child_process');
+const sp     = require('child_process');
+const os     = require('os');
+const fs     = require('fs');
+const colors = require('tmux-colors');
+const path   = require('path');
 
 // width of GPU usage
 const gpu_width = 5;
@@ -11,13 +15,15 @@ const pci_width = 3;
 const pwr_width = 2;
 const ram_width = 3;
 
+const filename = path.join(os.homedir(), '.gpu.tmp');
+
 // get the nvidia gpu ids installed
 var gpus = sp.spawnSync("nvidia-smi", ["--query-gpu=utilization.memory,utilization.gpu,power.draw,power.limit,memory.total,memory.used", "--format=csv,noheader,nounits"]);
 var output = gpus.stdout.toString();
 //console.log(output)
 var lines = output.split("\n");
 
-var samples = [[0, 0, 0, 0], [0, 0, 0, 0]];
+var samples = new Array(lines.length);
 var devices = 0;
 
 // for each gpu installed we'll query usage, temp and memory usage
@@ -25,6 +31,8 @@ lines.forEach(function(str){
     // [0] = ram util, [1] gpu util, [2] pwr draw, [3] pwr limit, [4] mem total [5] mem used
     var values = str.split(",");
     if(values.length != 6) return
+
+    samples[devices] = [0,0,0,0]
 
     samples[devices][0] = parseFloat(values[1]);
     samples[devices][1] = parseFloat(values[0]);
@@ -44,9 +52,7 @@ for(var i=0;i<devices;++i)
 {
     history_records[i] = {}
 }
-const fs = require('fs');
 var contents;
-var filename = '~/.gpu.tmp'
 // load previous samples from file
 fs.access(filename, fs.F_OK, function(err) {
     if (!err) {
@@ -107,7 +113,6 @@ function print_graphs(gpu_id)
         }
     }
 
-    var colors = require('tmux-colors');
     process.stdout.write(" #[fg=red,bold,bg=white]GPU" + gpu_id +" " + s[0].toFixed() + "% "+s[1].toFixed() + "% ")
     process.stdout.write(colors('#[fg=green,bold]'+text[0]+'#[fg=cyan,bold]'+text[1]
                          +'#[fg=red,bold]'+text[2]+'#[fg=yellow,bold]'+text[3]
